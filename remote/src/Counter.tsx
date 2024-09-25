4ce810236741b0a4d88a6d980ea25be1c9154906
Pavan A <pavan479ec@gmail.com>
2024-09-25 16:10:01 +0530

    initial commit

---
+++ remote/src/Counter.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
